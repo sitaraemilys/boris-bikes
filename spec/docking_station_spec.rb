4bd8eb20c31c713bfcb671a37c10ff166f6a853a
sitypop <sityemilyshah@gmail.com>
2016-04-12 15:11:56 +0100

    Added release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,15 @@
 # require "docking_station"
 
-# describe DockingStation do 
+# describe DockingStation do
 #   it "releases bike" do
 #     expect(release_bike).to eq "Bike released"
-#   end		
+#   end
 # end
 
 require "docking_station"
 
-RSpec.describe DockingStation do 
+RSpec.describe DockingStation do
   describe "release bike" do
   	it{should respond_to(:release_bike)}
-  end		
+  end
 end
