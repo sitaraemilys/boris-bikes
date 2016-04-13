794ddf764897a7845d98c00ddcf810d3581e3d01
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-04-13 10:39:04 +0100

    added tests to release bike and check if it works. Then passed these tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,13 @@ require 'docking_station'
 
 describe DockingStation do 	
 	it {is_expected.to respond_to :release_bike} 
+
+	it 'is of class Bike' do
+		expect(subject.release_bike).to be_instance_of Bike
+	end
+
+	it "has a working bike" do
+		expect(subject.release_bike.working?).to  eq true
+	end
 	
 end
