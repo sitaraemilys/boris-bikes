4ccb2e4c63a9cac84ace1980fb89ad3bbdf7384b
Sity <sity@Sitaras-MacBook-Air.local>
2016-04-13 14:23:16 +0100

    added test for for docking bike at docking station, updated method in docking station and passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,9 @@
 require 'docking_station'
 
-describe DockingStation do 	
-	it {is_expected.to respond_to :release_bike} 
+describe DockingStation do
+	it {is_expected.to respond_to :release_bike}
+	it {is_expected.to respond_to(:dock).with(1).argument }
+	it {is_expected.to respond_to (:bike)}
 
 	it 'is of class Bike' do
 		expect(subject.release_bike).to be_instance_of Bike
@@ -10,5 +12,12 @@ describe DockingStation do
 	it "has a working bike" do
 		expect(subject.release_bike.working?).to  eq true
 	end
-	
+
+it "return docked bikes" do
+	bike = Bike.new
+	subject.dock(bike)
+	expect(subject.bike).to eq bike
+end
+
+
 end
